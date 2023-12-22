270b98432b3279eb4468f04d577c924df6f75379
linkeando <linuxnanguelu@gmail.com>
2023-12-22 17:04:04 -0600

    update version vercel python

--- application/backend/routes/home.py
+++ application/backend/routes/home.py
@@ -6,9 +6,14 @@ from application.backend.services.message import Message
 
 home_bp = Blueprint('home', __name__)
 
+@home_bp.route('/', methods=['GET'])
+def view():
+    return 'test'
+
 
 @home_bp.route('/', methods=['POST'])
 def message_answer():
+    print('entro al post')
     data = request.get_json()
     message_user = data['message']['content']
     message = Message(sender="Bot", content=message_user, hour=datetime.now().strftime('%I:%M %p'))
